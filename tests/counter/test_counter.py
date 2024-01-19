33bcfad6a4318b1d99f27ce990fedcabad4305ba
Danilo Bellini <bellini.daan@gmail.com>
2024-01-19 15:33:35 -0300

    Commit inicial + implementa test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    result_python = count_ocurrences("data/jobs.csv", "Python")
+    result_javascript = count_ocurrences("data/jobs.csv", "Javascript")
+    assert result_python == 1639
+    assert result_javascript == 122
