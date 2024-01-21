ac58a1dbf26c7a2982bbec4e2ed36a08f9f72e5e
Danilo Bellini <bellini.daan@gmail.com>
2024-01-21 16:45:18 -0300

    feat: Implementa a filtragem de faixa de salarios

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,9 +23,44 @@ class ProcessSalaries(ProcessJobs):
         return min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError(
+                "Ambas as chaves 'min_salary' e 'max_salary' são necessárias"
+            )
+
+        if not (
+            str(job["min_salary"]).isdigit()
+            and str(job["max_salary"]).isdigit()
+        ):
+            raise ValueError(
+                "Os valores de 'min_salary' e 'max_salary' devem ser numéricos"
+            )
+
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+
+        if min_salary > max_salary:
+            raise ValueError(
+                "O valor de 'min_salary' não pode ser maior que 'max_salary'"
+            )
+
+        if not isinstance(salary, (int, str)):
+            raise ValueError("O salário deve ser um número ou string")
+
+        return min_salary <= int(salary) <= max_salary
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
     ) -> List[Dict]:
-        pass
+        good_salaried_jobs = []
+
+        for job in jobs:
+            try:
+                if self.matches_salary_range(job, salary):
+                    good_salaried_jobs.append(job)
+            except (
+                ValueError
+            ):  # Utiliza os erros do método matches_salary_range
+                continue
+
+        return good_salaried_jobs
