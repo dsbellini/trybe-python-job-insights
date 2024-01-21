4704de83f2f3a560450e848f5c298182d2b6ae11
Danilo Bellini <bellini.daan@gmail.com>
2024-01-21 16:54:40 -0300

    feat: Implementa testes dos empregos Brasileiros

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    original_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    translated_job = {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+    assert original_list[0] == translated_job
